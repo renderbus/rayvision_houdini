010fb33450fbdd9e3498f618e5e9cd12680ae2f1
dan <1113965383@qq.com>
2020-04-01 17:33:31 +0800

    feat: update houdini demo

--- help/examples/houdini_demo.py
+++ help/examples/houdini_demo.py
@@ -1,84 +1,98 @@
 # -*- coding: utf-8 -*-
-"""Create task demo.
+"""Houdini render demo."""
 
-1. Create a connection object.
-2. Request a task directly to get the task information.
-
-"""
-
-from __future__ import print_function
 from rayvision_api.core import RayvisionAPI
-from rayvision_utils.analyse_handle import RayvisionAnalyse
-from rayvision_api.task.handle import RayvisionTask
-from rayvision_api.task.check import RayvisionCheck
-from rayvision_sync.transfer import RayvisionTransfer
+from rayvision_houdini.analyze_houdini import AnalyzeHoudini
 from rayvision_sync.upload import RayvisionUpload
-from rayvision_sync.manage import RayvisionManageTask
 from rayvision_sync.download import RayvisionDownload
+from rayvision_api.task.check import RayvisionCheck
+from rayvision_api.utils import update_task_info, append_to_task, append_to_upload
 
-# Set necessary parameters
-
+# API Parameter
 render_para = {
-    "domain": "task.renderbus.com",  # 用戶不需要修改
-    "platform": "2",  # 平台号
-    "access_id": "xxxxxx",  # 用户自行修改(必填)
-    "access_key": "xxxxx",  # 用户自行修改(必填)
-    "local_os": 'windows',
-    "workspace": "c:/workspace",  # 本地保存根目录自动创建(用户可自行修改,全英文路径)
-    "render_software": "Houdini",  # CG软件（Maya, Houdini, Katana, Clarisse）
-    "software_version": "17.5.293",  # 注意CG版本的形式
-    "project_name": "Project1",
-    "plugin_config": {}  # CG插件，无插件则为{}
+    "domain": "task.renderbus.com",
+    "platform": "2",
+    "access_id": "xxxx",
+    "access_key": "xxxx",
 }
-
-# CG资源文件绝对路径(必须)
-cg_file = r"E:\copy\test02.hip"
 
 api = RayvisionAPI(access_id=render_para['access_id'],
                    access_key=render_para['access_key'],
                    domain=render_para['domain'],
                    platform=render_para['platform'])
 
-# 打印当前用户信息
-print(api.user.query_user_profile())
-
-# Rayvision analysis
-task = RayvisionTask(cg_file=cg_file, **render_para)
-RayvisionAnalyse.execute(task)
-RayvisionCheck(task).execute(task.task_info, task.upload_info)
-
-# Upload json file
-transfer_info = {
-    'config_bid': api.user_info['config_bid'],
-    'input_bid': api.user_info['input_bid'],
-    "output_bid": api.user_info["output_bid"],
-    "domain": render_para['domain'],
-    "platform": render_para['platform'],
-    "local_os": render_para['local_os'],
-    "user_id": api.user_info['user_id'],
-    "local_path": r"C:\workspace",  # 下载资源本地保存路径
+# Step1：Analyze CG File
+analyze_info = {
+    "cg_file": r"D:\files\CG FILE\flip_test_slice4.hip",
+    "workspace": "c:/workspace",
+    "software_version": "17.5.293",
+    "project_name": "Project1",
+    "plugin_config": {
+        'renderman': '22.6'
+    }
 }
+analyze_obj = AnalyzeHoudini(**analyze_info)
+analyze_obj.analyse()
 
-resource_config_file = {
-    "task_json_path": task.task_json_path,
-    "tips_json_path": task.tips_json_path,
-    "asset_json_path": task.asset_json_path,
-    "upload_json_path": task.upload_json_path,
+
+# step2: Add some custom parameters, or update the original parameter value
+update_task = {
+    "pre_frames": "100",
+    "stop_after_test": "1"
 }
+update_task_info(update_task, analyze_obj.task_json)
 
-# start transfer(传输)
-trans = RayvisionTransfer(**transfer_info)
-upload = RayvisionUpload(trans)
-upload.upload(task_id=task.task_id, **resource_config_file)
+custom_info_to_task = {
+    "env": "houdini_env"
+}
+append_to_task(custom_info_to_task, analyze_obj.task_json)
 
-task_id = int(task.task_id)
-result = api.submit(task_id)
+custom_info_to_upload = [
+    r"D:\files\CG FILE\Shark Test\home_test.build",
+    r"C:\workspace\1585303109\task.json"
+]
+append_to_upload(custom_info_to_upload, analyze_obj.upload_json)
 
-# download(下载)
-manage_task = RayvisionManageTask(api.query)
-trans.manage_task = manage_task
-download = RayvisionDownload(trans)
 
-# demo提供2种下载方式用户自行选择
-# download.auto_download_after_task_completed([task_id])  # 所有都完成后才自动开始统一下载
-download.auto_download([task_id])  # 轮询下载（每完成每一帧自动下载）
+# step3：Check json files
+check_obj = RayvisionCheck(api, analyze_obj)
+task_id = check_obj.execute(analyze_obj.task_json, analyze_obj.upload_json)
+
+
+# Step4: Transmission
+"""
+There are two ways to upload the transmission:
+Upload_method: 1: upload four json files and upload the resource file according to upload.json;
+               2: json files and resources are uploaded separately;
+"""
+CONFIG_PATH = {
+    "tips_json_path": analyze_obj.tips_json,
+    "task_json_path": analyze_obj.task_json,
+    "asset_json_path": analyze_obj.asset_json,
+    "upload_json_path": analyze_obj.upload_json,
+}
+upload_obj = RayvisionUpload(api)
+"""
+The default of the test demo is to upload json and resource files at the same time,
+and users can choose their own upload method according to the actual situation.
+"""
+upload_method = 1
+if upload_method == 1:
+    # step3.1：Json files are uploaded in conjunction with CG resources
+    upload_obj.upload(str(task_id), **CONFIG_PATH)
+elif upload_method == 2:
+    # step3.2：CG resource files and json are uploaded separately
+    upload_obj.upload_asset(upload_json_path=CONFIG_PATH["upload_json_path"])
+    upload_obj.upload_config(str(task_id), list(CONFIG_PATH.values()))
+
+
+# Step5：Submit Task
+api.submit(int(task_id))
+
+
+# Step6：Download
+download = RayvisionDownload(api)
+# All complete before the automatic start of uniform download.
+# download.auto_download_after_task_completed([task_id])
+# Poll download (automatic download for each completed frame)
+download.auto_download([int(task_id)])
